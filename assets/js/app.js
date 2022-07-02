e5346f839bd1f0c90ccf5112f1c66189ea03871a
Rikky Nguyen <34929740+rikkynguyen@users.noreply.github.com>
2022-07-02 16:05:11 +0700

    init new page

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+  $('.slide-portfolio').slick({
+    mobileFirst:true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 524,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 0,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  })
+})
